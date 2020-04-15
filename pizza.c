b4c24ff3576b494638c1913b56f38032c48e566b
sergei270791 <sergei.calle.c@uni.pe>
2020-04-14 23:22:50 -0500

    Version 0.0.1

---
+++ pizza.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf(" // ºº--.._");
+    printf("||  (_)  _ º-._");
+    printf("||    _ (_)    º-..");
+    printf("||   (_)   __..-º");
+    printf(" \\__..--ºº");
+    return 0;
+}
