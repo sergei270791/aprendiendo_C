d2f8cc6beef57b18a7a5a75a3a20919ba61f5140
sergei270791 <sergei.calle.c@uni.pe>
2020-04-15 22:23:01 -0500

    Version 0.1.3

---
+++ prob_datos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int CP, HP, Caramelos;
+    double Peso, Altura;
+    char Sexo;
+    Sexo= "M";
+    CP = 2764;
+    HP = 159;
+    Peso = 183.0;
+    Altura = 2.32;
+    Caramelos = 266;
+    printf("Sexo: %s\n CP: %d\n HP: %d\n Peso: %.1lf\n Altura: %.2lf\n Caramelos: %d\n",Sexo,CP,HP,Peso,Altura,Caramelos);
+    return 0;
+}
