a1959e42af0d3b613f660b3ecc02f781ea573df8
sergei270791 <sergei.calle.c@uni.pe>
2020-04-15 22:57:57 -0500

    version 0.1.4

--- prob_datos.c
+++ prob_datos.c
@@ -4,12 +4,18 @@ int main() {
     int CP, HP, Caramelos;
     double Peso, Altura;
     char Sexo;
-    Sexo= "M";
-    CP = 2764;
-    HP = 159;
-    Peso = 183.0;
-    Altura = 2.32;
-    Caramelos = 266;
-    printf("Sexo: %s\n CP: %d\n HP: %d\n Peso: %.1lf\n Altura: %.2lf\n Caramelos: %d\n",Sexo,CP,HP,Peso,Altura,Caramelos);
+    printf("Introduzca su sexo (M/F): ");
+    scanf("%c", &Sexo);
+    printf("Introduzca CP: ");
+    scanf("%d", &CP);
+    printf("Introduzca HP: ");
+    scanf("%d", &HP);
+    printf("Introduzca su peso: ");
+    scanf("%lf", &Peso);
+    printf("Introduzca su altura: ");
+    scanf("%lf", &Altura);
+    printf("Introduzca la cantidad de caramelos: ");
+    scanf("%d", &Caramelos);
+    printf("Sexo: %c\n CP: %d\n HP: %d\n Peso: %.1lf\n Altura: %.2lf\n Caramelos: %d\n",Sexo,CP,HP,Peso,Altura,Caramelos);
     return 0;
 }
