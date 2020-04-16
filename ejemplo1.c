a37a160bb5e59378b9b3a4d35c6cfe3b4d6cacd7
sergei270791 <sergei.calle.c@uni.pe>
2020-04-15 19:57:23 -0500

    Version 0.1.1

--- ejemplo1.c
+++ ejemplo1.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    double dolares,euros;
-    
-    dolares = 100;   
+    double euros;
+    double dolares;
+   
+    printf("Introduzca cantidad en dolares: ");
+    scanf("%lf", &dolares);
+   
     euros = dolares * 0.859;
-
     printf("%lf dolares son %lf euros.\n", dolares, euros);
    
     return 0;
---
+++ tiempo_series.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    double total,temporadas,capitulos,duracion,intro;
+    printf("Piensa en una serie que hayas visto...\n");
+    printf("¿Cuántas temporadas tiene? ");
+    scanf("%lf", &temporadas);
+    printf("¿Cuántos capítulos de media cada temporada? ");
+    scanf("%lf", &capitulos);
+    printf("¿Cuántos minutos duran aproximadamente los capítulos? ");
+    scanf("%lf", &duracion);
+    printf("¿Y su intro? ");
+    scanf("%lf", &intro);
+    total = temporadas * capitulos * (duracion-intro) / 60;
+    printf("¡En total has invertido unas %.2lf horas en ver la serie!",total);
+   
+    return 0;
+}
