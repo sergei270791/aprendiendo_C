96ba7b25748c0a5d4dd5c184ea1090b315894235
sergei270791 <sergei.calle.c@uni.pe>
2020-04-15 19:03:56 -0500

    Version 0.1.0

---
+++ ejemplo1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    double dolares,euros;
+    
+    dolares = 100;   
+    euros = dolares * 0.859;
+
+    printf("%lf dolares son %lf euros.\n", dolares, euros);
+   
+    return 0;
+}
